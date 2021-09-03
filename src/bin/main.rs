eb0187b0b9727f0504d0742a9b3558fb89d335fd
Stanley Su <stanleysu00@gmail.com>
2021-09-04 00:46:42 +1000

    Add linear blend gradient code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,48 @@
-use raytracing::{write_color, Color};
+use raytracing::{write_color, Color, Point3, Ray, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = Vec3::unit(r.direction());
+    // y value is now -1 <= y <= 1. Transform so 0 <= t <= 1
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    // at t = 1: output blue
+    // at t = 0: output white
+    // inbetween (0 < t < 1): linear blend
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
-    let color_max = 256;
+    // image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400u32;
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
+
+    // camera
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * aspect_ratio;
+    let focal_length = 1.0;
+
+    let origin = Point3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
 
     // PPM image format specifications
     println!("P3"); // colors are in ascii
     println!("{} {}", image_width, image_height);
-    println!("{}", color_max - 1);
+    println!("{}", 255);
 
     for y in (0..image_height).rev() {
         eprintln!("Scanlines remaining: {}", y + 1);
         for x in 0..image_width {
-            // redness increases from left to right (0 to 0.99...)
-            // greenness decreases from top to bottom (0.99... to 0)
-            let pixel_vec = Color::new(
-                x as f64 / image_width as f64,
-                y as f64 / image_height as f64,
-                0.7_f64,
+            let x_scale = x as f64 / (image_width - 1) as f64;
+            let y_scale = y as f64 / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + (horizontal * x_scale) + (vertical * y_scale),
             );
-            write_color(pixel_vec);
+            let pixel_color = ray_color(&r);
+            write_color(pixel_color);
         }
     }
 }
