4141656d5af9b9d0b2fff488b19c9a551d0bb76f
Stanley Su <stanleysu00@gmail.com>
2021-09-07 20:55:08 +1000

    Add distant view binary

--- src/camera.rs
+++ src/camera.rs
@@ -8,19 +8,17 @@ pub struct Camera {
 }
 
 impl Camera {
-    //  vertical_fov is in degrees
     pub fn new(
         look_from: Point3,
         look_at: Point3,
         vup: Vec3,
-        vertical_fov: f64,
+        vertical_fov: f64, // vertical field of view angle in degrees
         aspect_ratio: f64,
     ) -> Self {
         let theta = degrees_to_radians(vertical_fov);
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
 
         let w = Vec3::unit(look_from - look_at);
         let u = Vec3::unit(Vec3::cross(vup, w));
