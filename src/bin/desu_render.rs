5b376977c88fecf9071166412dfa4fc36eb09054
Stanley Su <stanleysu00@gmail.com>
2021-09-08 23:54:10 +1000

    Blue themed scene

--- src/bin/desu_render.rs
+++ src/bin/desu_render.rs
@@ -1,4 +1,3 @@
-use hsl::HSL;
 use raytracing::{
     rand_f64, rand_in_range, write_sampled_color, Camera, Color, Dieletric, Hittable, HittableList,
     Lambertian, Material, Metal, Point3, Ray, Sphere, Vec3,
@@ -15,57 +14,53 @@ fn random_scene() -> HittableList {
     )));
 
     let metal_mat = Rc::new(Metal::new(Color::new(1.0, 1.0, 1.0), 0.0));
-
-    let glass_mat = Rc::new(Dieletric::new(1.5));
-    let glass_p = Point3::new(4.0, 1.0, 1.0);
-    world.add(Rc::new(Sphere::new(glass_p, 1.0, glass_mat.clone())));
-    let glass_hollow = Point3::new(4.0, 1.0, 1.0);
-    world.add(Rc::new(Sphere::new(glass_hollow, -0.9, glass_mat.clone())));
-
-    let lamber_mat = Rc::new(Lambertian::new(Color::new(0.3, 0.3, 0.3)));
-    let lamber_p = Point3::new(-1.0, 1.2, -3.0);
-    world.add(Rc::new(Sphere::new(lamber_p, 1.2, lamber_mat.clone())));
-
-    let metal_p = Point3::new(-4.0, 1.0, 3.3);
+    let metal_p = Point3::new(-4.0, 1.0, 0.5);
     world.add(Rc::new(Sphere::new(metal_p, 1.0, metal_mat.clone())));
 
-    let look_from = Point3::new(16.0, 2.0, 3.0);
+    let glass_mat = Rc::new(Dieletric::new(1.5));
+    let glass_p = Point3::new(5.0, 1.0, 0.5);
+    world.add(Rc::new(Sphere::new(glass_p, 1.0, glass_mat.clone())));
+    let glass_hollow = Point3::new(5.0, 1.0, 0.5);
+    world.add(Rc::new(Sphere::new(glass_hollow, -0.9, glass_mat.clone())));
+
+    let lamber_mat = Rc::new(Lambertian::new(Color::new(1., 1., 1.)));
+    let lamber_p = Point3::new(-1.0, 1.0, -3.0);
+    world.add(Rc::new(Sphere::new(lamber_p, 1.0, lamber_mat.clone())));
+
+    let colors = vec![
+        Color::new(3., 4., 94.) / 255.,
+        Color::new(2., 62., 138.) / 255.,
+        Color::new(0., 119., 182.) / 255.,
+        Color::new(0., 150., 199.) / 255.,
+        Color::new(0., 180., 216.) / 255.,
+        Color::new(72., 202., 228.) / 255.,
+        Color::new(144., 224., 239.) / 255.,
+        Color::new(173., 232., 244.) / 255.,
+        Color::new(202., 240., 248.) / 255.,
+    ];
 
     for a in -11..11 {
         for b in -11..11 {
             let choose_mat = rand_f64();
-            let is_airborne = if choose_mat > 0.5 { 1.0 } else { 0.0 };
-            let vert_d = 0.2 + is_airborne * rand_in_range(0.0, 2.0);
             let mut center = Point3::new(
                 a as f64 + 0.7 * rand_f64(),
-                vert_d,
+                0.2,
                 b as f64 + 0.7 * rand_f64(),
             );
 
             let can_spawn = (center - glass_p).length() > 1.2
                 && (center - lamber_p).length() > 1.2
-                && (center - metal_p).length() > 1.2
-                && (center - look_from).length() > 8.0;
+                && (center - metal_p).length() > 1.2;
 
             if can_spawn {
                 let sphere_material: Rc<dyn Material>;
 
-                if choose_mat < 0.9 {
+                if choose_mat < 0.8 {
                     // diffuse
-                    let albedo_hsl = HSL {
-                        h: rand_in_range(0.0, 320.0),
-                        s: 1.0,
-                        l: 0.5,
-                    };
-                    let albedo_rgb = albedo_hsl.to_rgb();
-                    let albedo = Color::new(
-                        albedo_rgb.0 as f64 / 255.0,
-                        albedo_rgb.1 as f64 / 255.0,
-                        albedo_rgb.2 as f64 / 255.0,
-                    );
+                    let albedo = colors[(rand_in_range(0.0, colors.len() as f64)) as usize];
                     sphere_material = Rc::new(Lambertian::new(albedo));
                     world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
-                } else if choose_mat < 1.0 {
+                } else {
                     // glass
                     sphere_material = glass_mat.clone();
                     center[1] = 0.2; // glass balls on the ground
@@ -103,12 +98,12 @@ fn main() {
     let max_depth = 50u32;
 
     // camera
-    let look_from = Point3::new(16.0, 2.0, 3.0);
+    let look_from = Point3::new(13.0, 2.0, 3.0);
     let look_at = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = 10.0;
+    let dist_to_focus = (look_from - look_at).length();
     let aperture = 0.1;
-    let vertical_fov = 20.0;
+    let vertical_fov = 25.0;
     let cam = Camera::new(
         look_from,
         look_at,
